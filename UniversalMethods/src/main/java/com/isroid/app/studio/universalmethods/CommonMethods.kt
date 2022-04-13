0202006baecb5990c66c32c276310701676cb2e1
ISRAR <muhammadisrar152@gmail.com>
2022-04-13 23:25:47 +0500

    commit5

--- UniversalMethods/src/main/java/com/isroid/app/studio/universalmethods/CommonMethods.kt
+++ UniversalMethods/src/main/java/com/isroid/app/studio/universalmethods/CommonMethods.kt
@@ -1,8 +1,34 @@
 package com.isroid.app.studio.universalmethods
 
-import android.app.Activity
+import android.content.Context
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 
-fun Activity.showToast(message:String){
+typealias ids = R.id
+typealias strings = R.string
+typealias drawables = R.drawable
+typealias colors = R.color
+typealias dimens = R.dimen
+
+fun Context.showToast(message:String){
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun showLog(message:String,tag:String = "TESTING"){
+    if(BuildConfig.DEBUG){
+        Log.d(tag,message)
+    }
+}
+
+fun View.gone(){
+    visibility = View.GONE
+}
+
+fun View.show(){
+    visibility = View.VISIBLE
+}
+
+fun View.invisible(){
+    visibility = View.INVISIBLE
 }
