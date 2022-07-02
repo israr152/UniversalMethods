2d9d6524d146d4a613a77555310284b684f1518a
ISRAR <muhammadisrar152@gmail.com>
2022-07-02 11:44:11 +0500

    commit12 added dpToPixels and spToPixels

---
+++ UniversalMethods/src/main/java/com/isroid/app/studio/universalmethods/ViewExtensions.kt
@@ -0,0 +1,15 @@
+package com.isroid.app.studio.universalmethods
+
+import android.view.View
+
+fun View.gone(){
+    visibility = View.GONE
+}
+
+fun View.show(){
+    visibility = View.VISIBLE
+}
+
+fun View.invisible(){
+    visibility = View.INVISIBLE
+}
